68fde141596269d30876871f5931b2dc17ae6c99
Alejandro Reyes <rareyesdev@gmail.com>
2023-08-26 23:48:55 -0700

    feat: :construction: add non-featured quotes data

---
+++ data/notFeaturedQuotesData.ts
@@ -0,0 +1,9 @@
+const notFeaturedQuotesData = [
+  {
+    quote:
+      "Testing by itself does not improve software quality. Test results are an indicator of quality, but in and of themselves, they don't improve it. Trying to improve software quality by increasing the amount of testing is like trying to lose weight by weighing yourself more often. What you eat before you step onto the scale determines how much you will weigh, and the software development techniques you use determine how many errors testing will find. If you want to lose weight, don't buy a new scale; change your diet. If you want to improve your software, don't test more; develop better.",
+    author: 'Code Complete',
+  },
+]
+
+export default notFeaturedQuotesData
