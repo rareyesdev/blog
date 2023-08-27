5e2d779be60c7e6483e8bf8c1ea8d72296cf3bd8
Alejandro Reyes <rareyesdev@gmail.com>
2023-08-26 23:51:01 -0700

    feat: :construction: add featured quote to the main page

--- app/Main.tsx
+++ app/Main.tsx
@@ -6,7 +6,7 @@ import NewsletterForm from 'pliny/ui/NewsletterForm'
 
 const MAX_DISPLAY = 5
 
-export default function Home({ posts }) {
+export default function Home({ posts, featuredQuote }) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -14,8 +14,8 @@ export default function Home({ posts }) {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            {siteMetadata.description}
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400 italic">
+            "{featuredQuote}"
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
+import featuredQuotesData from '@/data/featuredQuotesData'
 import Main from './Main'
 
 export default async function Page() {
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+  const featuredQuote =
+    featuredQuotesData[Math.floor(Math.random() * featuredQuotesData.length)].quote
+
+  return <Main posts={posts} featuredQuote={featuredQuote} />
 }
