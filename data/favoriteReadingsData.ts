0b31a8d5eb981f62b3e4a174f9f6b8cee875204e
Alejandro Reyes <rareyesdev@gmail.com>
2023-08-26 23:45:20 -0700

    feat: :construction: add readings and quotes data

---
+++ data/favoriteReadingsData.ts
@@ -0,0 +1,75 @@
+const favoriteReadingsData = {
+  platinum: [
+    {
+      title: '12 Rules for Life',
+      author: ['Jordan B. Peterson'],
+    },
+    {
+      title: 'Code Complete (2nd Edition)',
+    },
+    {
+      title: 'The DevOps Handbook',
+    },
+  ],
+  gold: [
+    {
+      title: 'Meditations',
+      author: ['Marcus Aurelius'],
+    },
+    {
+      title: 'The Humble Programmer',
+      author: ['Edsger W. Dijkstra'],
+    },
+    {
+      title: "You Don't Know JS",
+      author: ['Kyle Simpson'],
+    },
+    {
+      title: 'How to Win Friends and Influence People',
+      author: ['Dale Carnegie'],
+    },
+  ],
+  silver: [
+    {
+      title: 'You and Your Research',
+      author: ['Richard Hamming'],
+      url: 'https://homepages.inf.ed.ac.uk/wadler/papers/firbush/hamming.pdf',
+    },
+    {
+      title: 'Atomic habits',
+      author: ['James Clear'],
+    },
+    {
+      title: "The Millionaire Next Door: The Surprising Secrets of America's Rich",
+      author: ['Thomas J. Stanley', 'William D. Danko'],
+    },
+    {
+      title: 'Rich Dad Poor Dad',
+      author: ['Robert Kiyosaki', 'Sharon Lechter'],
+    },
+    {
+      title: 'You Can Negotiate Anything',
+      author: ['Herb Cohen'],
+    },
+    {
+      title: 'Cracking the Coding Interview',
+      author: ['Gayle Laakmann McDowell'],
+    },
+  ],
+  bronze: [
+    {
+      title: 'Predictably Irrational',
+      author: ['Dan Ariely'],
+    },
+    {
+      title: 'The Pragmatic Programmer',
+      author: ['Andy Hunt', 'Dave Thomas'],
+    },
+    {
+      title: '97 Things Every Programmer Should Know: Collective Wisdom from the Experts',
+      author: ['Kevlin Henney'],
+    },
+  ],
+}
+
+export default favoriteReadingsData
---
+++ data/featuredQuotesData.ts
@@ -0,0 +1,85 @@
+const featuredQuotesData = [
+  {
+    quote: 'General Principle of Software Quality: Improving quality reduces development costs',
+    author: 'Code Complete',
+  },
+  {
+    quote:
+      'The competent programmer is fully aware of the strictly limited size of his own skull; therefore, he approaches the programming task in full humility',
+    author: 'Edsger W. Dijkstra',
+  },
+  {
+    quote: "Software's primary technical imperative is to manage complexity",
+    author: 'Code Complete',
+  },
+  {
+    quote:
+      'The best is the enemy of the good. Working toward perfection might prevent completion. Complete it first, and then perfect it',
+    author: 'Code Complete',
+  },
+  {
+    quote:
+      'We should forget about small efficiencies, say about 97% of the time: premature optimization is the root of all evil',
+    author: 'Donald Knuth',
+  },
+  {
+    quote: "Don't change the code randomly. That's voodoo programming",
+  },
+  {
+    quote: 'The Best Code is No Code At All',
+    author: 'Jeff Atwood',
+  },
+  {
+    quote:
+      'Any fool can write code that a computer can understand. Good programmers write code that humans can understand',
+    author: 'Martin Fowler',
+  },
+  {
+    quote:
+      "Perfect may be the enemy of good, but it can also be the friend of better. Perfection is debilitating when we see it as a target that we failed to reach. It's motivating when we recognize it as an ideal that's worth striving for even though it can't be reached",
+  },
+  {
+    quote: 'Never attribute to malice that which is adequately explained by stupidity',
+  },
+  {
+    quote:
+      'May you be granted the serenity to accept the things you cannot change, the courage to change the things you can, and the wisdom to know the difference',
+  },
+  {
+    quote:
+      'If any man is able to convince me and show me that I do not think or act right, I will gladly change; for I seek the truth by which no man was ever injured. But he is injured who abides in his error and ignorance',
+    author: 'Marcus Aurelius',
+  },
+  {
+    quote:
+      'Now, here, you see, it takes all the running you can do, to keep in the same place. If you want to get somewhere else, you must run at least twice as fast as that!',
+    author: 'Red Queen',
+  },
+  {
+    quote:
+      'We are all captives of the picture in our head - our belief that the world we have experienced is the world that really exists',
+    author: 'Walter Lippmann',
+  },
+  {
+    quote:
+      'Somebody once told me the definition of hell: On your last day on earth, the person you became will meet the person you could have become',
+  },
+  {
+    quote:
+      'There is a lot going on, so that you can sit here and pretend that you know enough to stay alive',
+    author: 'Jordan Peterson',
+  },
+  {
+    quote: 'We make our fortunes and call them fate',
+    author: 'Benjamin Disraeli',
+  },
+  {
+    quote: 'The further a society drifts from the truth the more it will hate those who spreak it',
+  },
+  {
+    quote: 'The things you own end up owning you',
+    author: 'Tyler Durden, Fight Club',
+  },
+]
+
+export default featuredQuotesData
