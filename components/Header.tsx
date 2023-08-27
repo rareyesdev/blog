89411723de2552a6b7c217f066f9388349cdfea8
Alejandro Reyes <rareyesdev@gmail.com>
2023-08-26 23:48:34 -0700

    feat: :construction: add logo to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
+import Image from './Image'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
@@ -13,7 +13,12 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Logo />
+              <Image
+                width={50}
+                height={50}
+                src="/static/favicons/android-chrome-96x96.png"
+                alt="logo"
+              />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
