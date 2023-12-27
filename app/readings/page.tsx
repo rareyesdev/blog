3061f4cc57102f121a5a3cbfa2d8cef363493e65
Alejandro Reyes <rareyesdev@gmail.com>
2023-12-27 15:14:44 -0800

    feat: :construction: rename readings page

--- app/readings/page.tsx
+++ app/readings/page.tsx
@@ -1,6 +1,6 @@
 import { Authors, allAuthors } from 'contentlayer/generated'
 import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import AuthorLayout from '@/layouts/AuthorLayout'
+import PostLayout from '@/layouts/PostLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 import { components } from '@/components/MDXComponents'
@@ -8,14 +8,9 @@ import { components } from '@/components/MDXComponents'
 export const metadata = genPageMetadata({ title: 'Readings' })
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
-
   return (
-    <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} components={components} />
-      </AuthorLayout>
-    </>
+    <div>
+      <div>favorite readings</div>
+    </div>
   )
 }
