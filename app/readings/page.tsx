4718489e9257f555fbcb7a5acfac9c4d205c7c15
Alejandro Reyes <rareyesdev@gmail.com>
2023-12-27 18:13:14 -0800

    feat: :construction: add readings page

--- app/readings/page.tsx
+++ app/readings/page.tsx
@@ -1,16 +1,25 @@
-import { Authors, allAuthors } from 'contentlayer/generated'
-import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import PostLayout from '@/layouts/PostLayout'
-import { coreContent } from 'pliny/utils/contentlayer'
+import favoriteReadingsData from '@/data/favoriteReadingsData'
 import { genPageMetadata } from 'app/seo'
-import { components } from '@/components/MDXComponents'
+import { Shelf } from './shelf'
 
 export const metadata = genPageMetadata({ title: 'Readings' })
 
 export default function Page() {
   return (
-    <div>
-      <div>favorite readings</div>
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Favorite Readings
+        </h1>
+        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          This is a list of my favorite readings (most are books with a few exceptions). It is
+          organized in platinum, gold, silver and bronze. I don't put every book that I read, only
+          the best ones make it.
+        </p>
+      </div>
+      <div className="container py-12">
+        <Shelf favoriteReadingsData={favoriteReadingsData} />
+      </div>
     </div>
   )
 }
---
+++ app/readings/shelf.tsx
@@ -0,0 +1,64 @@
+import React, { FC, useMemo } from 'react'
+import { joinClasses } from '../../utils/join-classes'
+import { MedalIcon } from './medal-icon'
+import { BookIcon } from './book-icon'
+import Link from '../../components/Link'
+
+const colors = {
+  platinum: '#ffffff',
+  gold: '#ffd300',
+  silver: '#9a9a9a',
+  bronze: '#ab7627',
+}
+
+type ShelfProps = {
+  favoriteReadingsData: any
+}
+
+export const Shelf: FC<ShelfProps> = ({ favoriteReadingsData }) => {
+  const rowNames = useMemo(() => Object.keys(favoriteReadingsData), [favoriteReadingsData])
+
+  return (
+    <div>
+      {rowNames.map((rowName) => (
+        <ShelfRow key={rowName} readings={favoriteReadingsData[rowName]} color={colors[rowName]} />
+      ))}
+    </div>
+  )
+}
+
+type Reading = {
+  title: string
+  url?: string
+}
+
+type ShelfRowProps = {
+  color: string
+  readings: Reading[]
+}
+
+const ShelfRow: FC<ShelfRowProps> = ({ color, readings }) => {
+  return (
+    <div className="my-12 md:flex md:items-center">
+      <MedalIcon className="h-14 mb-4 mx-auto md:m-0 md:mr-10" style={{ color }} />
+      <ul className="m-0 p-0 list-none">
+        {readings.map((reading) => {
+          const item = (
+            <li key={reading.title} className="relative leading-loose">
+              <BookIcon className="absolute translate-y-2/4 h-3 top-1" />
+              <div className="ml-5">{reading.title}</div>
+            </li>
+          )
+
+          return reading.url ? (
+            <Link href={reading.url} target="_blank" rel="noopener noreferrer">
+              {item}
+            </Link>
+          ) : (
+            item
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
