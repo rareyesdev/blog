9c73f518e9e119cc3c729d78313b3d866fba833b
Alejandro Reyes <rareyesdev@gmail.com>
2023-12-30 13:13:39 -0800

    feat: increase search icon thickness

--- components/SearchButton.tsx
+++ components/SearchButton.tsx
@@ -16,7 +16,7 @@ const SearchButton = () => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          strokeWidth={1.5}
+          strokeWidth={2.3}
           stroke="currentColor"
           className="h-6 w-6 link-highlighted text-light-foreground-deeper dark:text-dark-foreground-deeper"
         >
