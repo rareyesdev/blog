41088d19960fbe3d0239d773794958f242594215
Alejandro Reyes <rareyesdev@gmail.com>
2024-04-18 23:26:45 -0700

    remove tags from navigation

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  // { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/readings', title: 'Readings' },
   { href: '/quotes', title: 'Quotes' },
